449b86b7d282e37953855cb91b3a51d8505b3a86
Mike Harmon <mdh266@gmail.com>
2024-12-23 21:55:21 -0500

    adding debugging and caching the api calls

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_groq import ChatGroq
 import os
 from typing import Iterator, List, Dict, Tuple
-
+from dataclasses import dataclass
 
 
 def tuplify(history: List[Dict[str, str]]) -> List[Tuple[str, str]]:
     return [(d['role'], d['content']) for d in history]
 
 
-def ask_question(llm: ChatGroq, history: List[Tuple[str, str]], question: str) -> str:
+@st.cache_data
+def ask_question(history: List[Tuple[str, str]], question: str) -> str:
+    llm = ChatGroq(
+            model="llama-3.3-70b-versatile",
+            temperature=0,
+            max_tokens=None,
+            timeout=None,
+            max_retries=2)
+    
     prompt = ChatPromptTemplate.from_messages(
         [
             ("system", "You are a helpful assistant."),
@@ -34,6 +42,7 @@ def ask_question(llm: ChatGroq, history: List[Tuple[str, str]], question: str) -
     return response.content
 
 
+@st.cache_data
 def translate_text(target: str, text: str, google_api: str) -> Dict[str, str]:
 
     translate_client = translate.Client(client_options={"api_key": google_api})
@@ -45,16 +54,19 @@ def translate_text(target: str, text: str, google_api: str) -> Dict[str, str]:
     # will return a sequence of results for each text.
     result = translate_client.translate(text, target_language=target)
 
-
     return result
 
+
+@st.cache_data
 def text_to_speech(
     language_code: str,
     name: str,
-    tts: texttospeech.TextToSpeechClient,
+    google_api: str,
     translated_answer: str
 ) -> texttospeech_v1.types.cloud_tts.SynthesizeSpeechResponse:
     
+    tts = texttospeech.TextToSpeechClient(client_options={"api_key": google_api})
+
     synthesis_input = texttospeech.SynthesisInput(text=translated_answer)
 
     voice = texttospeech.VoiceSelectionParams(
@@ -71,11 +83,14 @@ def text_to_speech(
     return response
 
 
+@st.cache_data
 def speech_to_text(
     language_code: str,
-    stt: speech.SpeechClient,
+    google_api: str,
     question: st.runtime.uploaded_file_manager.UploadedFile
 ) -> str:
+
+    stt = speech.SpeechClient(client_options={"api_key": google_api})
 
     stt_config = speech.RecognitionConfig(
             encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
@@ -90,33 +105,29 @@ def speech_to_text(
     return text_question
 
 
-def main():
+def create_audio():
+    return st.audio_input("Click the microphone to start and stop recording")
+
+
+def clear_session():
+    st.session_state.messages = []
+    st.session_state.english_messages = []
+    st.session_state.transcribe = None
+    st.session_state.text_question = None
+    st.session_state.question = None
+
+
+def main(debug: bool = False):
+    if debug:
+        from dotenv import load_dotenv
+        load_dotenv()
 
     groq_api = os.getenv("GROQ_API_KEY")
     google_api = os.getenv("GOOGLE_API_KEY")
 
-    def clear_session():
-        st.session_state.messages = []
-        st.session_state.english_messages = []
-        st.session_state.transcribe = False
-        question = None
-        text_question = None
-        english_question = None
-        answer = None
-        translated_answer = None
-
     if "messages" not in st.session_state:
         clear_session()
-
-    llm = ChatGroq(
-                model="llama-3.3-70b-versatile",
-                temperature=0,
-                max_tokens=None,
-                timeout=None,
-                max_retries=2)
     
-    stt = speech.SpeechClient(client_options={"api_key": google_api})
-    tts = texttospeech.TextToSpeechClient(client_options={"api_key": google_api}) 
     
     col1, col2 = st.columns(2, gap="large")
 
@@ -126,42 +137,48 @@ def main():
         st.session_state.transcribe = st.checkbox("Transcribe Conversation", key="disabled")
     
         st.markdown("Ask A Question:")
-        question = st.audio_input("Click the microphone to start and stop recording")
+        st.session_state.question = create_audio()
 
-        if question:
+        if st.session_state.question:
             if st.session_state.human_language == "English":
                 stt_language_code="en-US" 
             else:
                 stt_language_code="he-IL"
 
-            text_question = speech_to_text(
+            st.session_state.text_question = speech_to_text(
                 language_code=stt_language_code,
-                stt=stt,
-                question=question)
-
-            st.session_state.messages.append({"role": "human", 
-                                              "content": f"Human: {text_question}"})
+                google_api=google_api,
+                question=st.session_state.question)
+            
+            st.session_state.messages.append({
+                "role": "human", 
+                "content": f"Human: {st.session_state.text_question}"
+            })
             
             if st.session_state.human_language == "Hebrew":
-                english_question = translate_text("en", 
-                                                  text_question, 
-                                                  google_api).get("translatedText")
+                english_question = translate_text(target="en", 
+                                                text=st.session_state.text_question, 
+                                                google_api=google_api).get("translatedText")
             else:
-                english_question = text_question
+                english_question = st.session_state.text_question
 
             if english_question:
                 history = tuplify(st.session_state.english_messages)
 
-                st.session_state.english_messages.append({"role": "human", 
-                                                          "content": f"Human: {english_question}"})
+                st.session_state.english_messages.append({
+                    "role": "human", 
+                    "content": f"Human: {english_question}"
+                })
                 
-                answer = ask_question(llm=llm, history=history, question=english_question)
+                answer = ask_question(history=history, question=english_question)
                 
                 st.session_state.english_messages.append({"role": "ai", 
-                                                          "content": f"Bot: {answer}"})
+                                                        "content": f"Bot: {answer}"})
 
                 if st.session_state.ai_language == "Hebrew":
-                    translated_answer = translate_text("he", answer ).get("translatedText")
+                    translated_answer = translate_text(target="he", 
+                                                    text=answer, 
+                                                    google_api=google_api).get("translatedText")
                     
                     tts_language_code = "en-US"
                     tts_name = "en-US-Journey-A"
@@ -170,14 +187,16 @@ def main():
                     tts_language_code = "he-IL"
                     tts_name = "he-IL-Standard-A"
 
-                st.session_state.messages.append({"role": "ai", 
-                                                  "content": f"Bot: {translated_answer}"})
+                st.session_state.messages.append({
+                    "role": "ai", 
+                    "content": f"Bot: {translated_answer}"
+                })
 
 
                 response = text_to_speech(language_code=tts_language_code,
-                                          name=tts_name,
-                                          tts=tts,
-                                          translated_answer=translated_answer)
+                                        name=tts_name,
+                                        google_api=google_api,
+                                        translated_answer=translated_answer)
 
                 if response:
                     st.markdown("Play AI Response:")
@@ -189,23 +208,24 @@ def main():
         with col2:
             if st.session_state.transcribe:
                     # Display chat messages from history on app rerun
+                
                 for message in st.session_state.messages:
                     with st.chat_message(message["role"]):
-                         st.markdown(message["content"])
+                            st.markdown(message["content"])
 
-            # css='''
-            # <style>
-            #     section.main>div {
-            #         padding-bottom: 1rem;
-            #     }
-            #     [data-testid="column"]>div>div>div>div>div {
-            #         overflow: auto;
-            #         height: 70vh;
-            #     }
-            # </style>
-            # '''
+        # css='''
+        # <style>
+        #     section.main>div {
+        #         padding-bottom: 1rem;
+        #     }
+        #     [data-testid="column"]>div>div>div>div>div {
+        #         overflow: auto;
+        #         height: 70vh;
+        #     }
+        # </style>
+        # '''
 
-            # st.markdown(css, unsafe_allow_html=True)
+        # st.markdown(css, unsafe_allow_html=True)
 
 if __name__ == "__main__":
-    main()
+    main(debug=True)
